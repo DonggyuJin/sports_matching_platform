5a15bce0282f88b5de4f90294a25478b81d7c46c
DonggyuJin <jdk2531@gmail.com>
2022-08-16 00:20:26 +0900

    update

--- sports/views/answer_views.py
+++ sports/views/answer_views.py
@@ -5,6 +5,6 @@ from ..models import FreeContent, FreeAnswer
 
 def answer_create(request, freeContent_id):
     freeContent = get_object_or_404(FreeContent, pk=freeContent_id)
-    freeContent.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    freeContent.freeanswer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('sports:detail', freeContent_id=freeContent.id)
 
